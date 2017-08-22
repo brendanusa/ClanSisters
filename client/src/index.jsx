f5058e39ef6fcd40fb03674d4bc52088b60b55df
Samuel Davis <davisss64@gmail.com>
2017-08-22 11:09:11 -0700

    Nuked the existing client.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
 import { BrowserRouter as Router} from 'react-router-dom';
+
+/*Eventually these will need to be dynamic routes. I think that we
+
+
+*/
+
+const App = () => (
+  <div>
+    <Switch>
+      <Route exact path = '/' >
+        <Home />
+      </Route>
+      <Route exact path = '/login' >
+        <Login />
+      </Route>
+      <Route exact path = '/user:userID' >
+        <User />
+      </Route>
+      <Route exact path = '/clan:clanID' >
+       <Clan />
+       </Route>
+      <Route exact path ='/forum' >
+       <Forum />
+      </Route>
+    </Switch>
+  </div>
+
+)
 
 /** Render App using React Router. */
 ReactDOM.render((
