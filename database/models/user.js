9ccc640f9608979cfaea3b85a014635885a7d53f
Thomas Volk <tvolk131@gmail.com>
2017-08-22 18:28:41 -0700

    User model stores email and screen name instead of username, and no longer uses separate salt

--- database/models/user.js
+++ database/models/user.js
@@ -1,20 +1,9 @@
-/**
- * @todo Switch from sha256 to bcrypt
- */
-
-const crypto = require('crypto');
 const {Sequelize, db} = require('../connection');
 const Clan = require('./clan');
 const Post = require('./post');
 
-const hashData = (data, salt = '') => {
-  let shasum = crypto.createHash('sha256');
-  shasum.update(data + salt);
-  return shasum.digest('hex');
-};
-
 const UserModel = db.define('user', {
-  username: {
+  email: {
     type: Sequelize.STRING(40),
     allowNull: false,
     unique: true
@@ -23,30 +12,21 @@ const UserModel = db.define('user', {
     type: Sequelize.STRING(64),
     allowNull: false
   },
-  salt: {
-    type: Sequelize.STRING(64),
-  }
-}, {
-  hooks: {
-    beforeCreate: (user) => {
-      user.salt = crypto.randomBytes(32).toString('hex');
-      user.password = hashData(user.password, user.salt);
-    }
+  screen_name: {
+    type: Sequelize.STRING(40),
+    allowNull: false
   }
 });
 
 UserModel.prototype.toJSON = function() {
   let values = this.dataValues;
-
   delete values.password;
-  delete values.salt;
-
   return values;
 };
 
 let User = {model: UserModel};
 
-const privateData = ['password', 'salt'];
+const privateData = ['password'];
 
 const joinArray = [
   {
@@ -58,37 +38,6 @@ const joinArray = [
     association: 'posts',
   },
 ];
-
-User.create = ({username, password}) => {
-  return UserModel.findOrCreate({
-    where: {username},
-    defaults: {password}
-  })
-    .spread((user, created) => {
-      if (!created) {
-        throw new Error('User already exists');
-      }
-
-      return user.reload({
-        include: joinArray
-      });
-    })
-    .then(user => user.toJSON());
-};
-
-User.validate = ({username, password}) => {
-  return UserModel.findOne({where: {username}})
-    .then((user) => {
-      if (user && user.password === hashData(password, user.salt)) {
-        return user.reload({
-          include: joinArray
-        })
-          .then(user => user.toJSON());
-      } else {
-        return null;
-      }
-    });
-};
 
 User.findAll = (query = {}) => {
   return UserModel.findAll({
@@ -114,7 +63,6 @@ User.read = User.find = (query = {}) => {
 
 /**
  * @todo test coverage for update!
- * @todo generate new password hash?
  * Model.update returns an array with the affected row count and the rows affected.
  * It should be decided how that is returned later.
  */
