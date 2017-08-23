f2dfba31870c4344cac30fa92b4f380a1a9ee228
Samuel Davis <davisss64@gmail.com>
2017-08-23 16:25:17 -0700

    Started scaffolding on clan page

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from './page-components/homePage.jsx';
+import Home from './page-components/HomePage.jsx';
 import Login from './login/index.jsx';
-import User from './page-components/userPage.jsx';
-import Clan from './page-components/clanPage.jsx';
-import Forum from './page-components/forumPage.jsx';
+import User from './page-components/UserPage.jsx';
+import Clan from './page-components/ClanPage.jsx';
+import Forum from './page-components/ForumPage.jsx';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './styles.css';
 /* 
@@ -14,8 +14,15 @@ As of right now there are a few pages scaffoled with fake data.
 They are not really formatted in any way, I have just added in the 
 Material-UI components, along with instructions as to how they should be
 hooked up to live data. Page formatting and visual organization is not implemented. 
+Doing css styling is merely cosmetic and can be added later after we get functional. 
 
 note: Disabled dynamic routing for testing purposes. 
+
+Captains Log: StarDate 8/23/2017-3:36PM. 
+Scaffolding progress is probably about 2/3 finished. The clans and forum pages still need to
+be added. I am quite happy with the way that the material-ui looks so far, but I do think
+that the forum page might need a different ui, as I'm not sure that there's really a good 
+material-ui component for this. 
 */
 
 
