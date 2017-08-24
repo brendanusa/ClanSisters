7bc5877401b96d257ff2047cc495614d98538a5c
Samuel Davis <davisss64@gmail.com>
2017-08-23 20:36:57 -0700

    Finished scaffold of users page.

---
+++ client/src/components/ShowForums.jsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
+import ForumCard from './ForumCard.jsx';
+/*
+I am going to use the map method here in order to populate clan forums. 
+This may not be the best way to implement this within the scope of Redux.  
+Redux-ers, please feel free to change this schematic as you see needed. 
+
+The dialog box should lead us to a form where we can create a new forum. 
+Eventually, a lot of stuff here may need re-written with redux. 
+
+*/
+
+const clanForums = [
+    {title: 'Test', heading: 'Test Heading', id:'001'},
+    {title: 'Test2', heading: 'Test Heading 2', id: '002'},
+    {title: 'Test3', heading: 'Test Heading 3', id: '003'}
+
+]
+
+
+const forumScrollbox = {
+    height: '200px',
+    width: '500px',
+    border: '5px',
+    overflow: 'auto',
+}
+
+
+export default class ShowForums extends React.Component {
+    constructor (props ) {
+      super(props)
+      this.state = {
+        open: false,
+      };
+
+      
+  const handleOpen = () => {
+    console.log('opening')
+    this.setState({open: true});
+  };
+
+  const handleClose = () => {
+    console.log('closing')
+    this.setState({open: false});
+  };
+}
+    render () {
+        const actions = [
+      <FlatButton
+        label="Cancel"
+        primary={true}
+        onClick={this.handleClose}
+      />,
+      <FlatButton
+        label="Submit"
+        primary={true}
+        keyboardFocused={true}
+        onClick={this.handleClose}
+      />,
+    ];
+    return (
+    <div>
+      <div style = {forumScrollbox} >
+        {clanForums.map(forum =>
+        <ForumCard forum = {forum} key= {forum.id} />
+        )}
+      </div>
+      <div>
+      <RaisedButton label="Add a Forum" onClick={this.handleOpen} />
+        <Dialog
+          title="New Forum"
+          actions={actions}
+          modal={false}
+          open={this.state.open}
+          onRequestClose={this.handleClose}
+        >
+          Eventually, this will have forms that will allow us to make a clan.
+        </Dialog>
+      </div>
+    </div>
+    )
+    }
+}
--- client/src/pageComponents/ClanPage.jsx
+++ client/src/pageComponents/ClanPage.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ShowForums from '../components/ShowForums.jsx';
+import AutoComplete from 'material-ui/AutoComplete';
+import ForumUserList from '../components/ForumUserList.jsx';
 import RaisedButton from 'material-ui/RaisedButton';
 
 /*
@@ -11,6 +13,8 @@ could have clans have their own avatars.
 When the button is pressed we should be able to join the clan. 
 More styling can be added pretty easily, look at the Material-ui docs. 
 
+The Autocomplete that we have here should return the same results as the
+the autocomplete from the home page. 
 
 
 */
@@ -18,6 +22,20 @@ More styling can be added pretty easily, look at the Material-ui docs.
 const joinClan = () => {
     alert('NUCLEAR LAUNCH IN 5, 4, 3...')
 }
+
+const testClans = [
+  'Starcraft',
+  'Mass Effect',
+  'FIFA',
+  'Crysis',
+  'Battlefield',
+  'Destiny'
+]
+
+const menuProps = {
+  desktop: true,
+  disableAutoFocus: true,
+};
 
 const Clan = (props) => {
     return (
@@ -29,6 +47,20 @@ const Clan = (props) => {
             onClick = {joinClan}
             />
             </div>
+        <div>
+           <ShowForums /> 
+        </div>
+        <div>
+            <AutoComplete         
+              hintText="Find a clan!!"
+              dataSource={testClans}
+              menuProps={menuProps}
+              />
+        </div>
+        <div>
+            <ForumUserList /> 
+        </div>
+
         </div>
     )
 }
