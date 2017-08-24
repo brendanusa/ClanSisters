79435b278b2c8a268d6eb8cdc01e97a9728035aa
OneCent01 <jmpenney22@gmail.com>
2017-08-24 08:54:55 -0700

    configuring state changes, reducer cleanup

--- client/src/configureStore.js
+++ client/src/configureStore.js
@@ -2,10 +2,22 @@ import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { rootReducer } from './reducers/clanReducers.js'
 
+const initialState = {
+  chatrooms: [],
+  users: [],
+  currentUser: null,
+  clans: [],
+  currentClan: null,
+  chatrooms: [],
+  currentChatroom: null,
+  membersOnline: [],
+  posts: []
+}
 
 export default function configureStore() {
   let store =  createStore(
     rootReducer,
+    initialState,
     applyMiddleware(thunk)
   )
   return store;
--- client/src/reducers/clanReducers.js
+++ client/src/reducers/clanReducers.js
@@ -13,23 +13,12 @@ const initialState = {
 }
 
 export const rootReducer = (state=initialState, action) => {
-  
-  // switch(action.type)
-  //   case(types.FETCH_ALL_CLANS): {
-  //     //is the action being returned the one passed into the fetchClansReducer
-  //     //function, or is it the result of invoking the action creator?
-  //     return {...state, action.payload}
-  //   }
-
-  // How is the action creator being invoked? 
-  // in the examples, such as that seen above, it seems like Redux invokes the
-  // action creator corresponding with the action type before returning the new state
   let newState;
   if(action.type === types.FETCH_ALL_CLANS) {
     newState = {...state, clans: action.payload}
   } else if (action.type === types.FETCH_USER_CLANS) {
     newState = {...state, members: action.payload}
   }
-  return newState
+  return newState || state;
 }
 
