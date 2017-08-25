46bbcfae0db7d7bd18d2b54a31d4da083babf8fe
Samuel Davis <davisss64@gmail.com>
2017-08-24 19:35:37 -0700

    Added more to babel.

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,9 @@ module.exports = {
         loader: 'babel-loader',
         query: {
           presets: ['react', 'es2015', 'stage-2'],
-          "plugins": ["transform-object-rest-spread"]
+          "plugins": [
+          ["transform-object-rest-spread", { "useBuiltIns": true }]
+          ]
         }
       },
       {
